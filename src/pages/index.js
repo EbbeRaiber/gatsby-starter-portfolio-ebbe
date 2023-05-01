ab6edbab5f93d12403fe8d2a6a6fc5543bac0ff7
EbbeRaiber <ebberaiber@outlook.com>
2023-05-01 15:57:10 -0400

    Third commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Ebbe's making this by following the Gatsby Tutorial.</p>
       </Layout>
     </main>
   )
